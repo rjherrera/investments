bbff3a5ce9e39c8240c8811d5bfdb871f598dd08
Javier López <jilopez8@uc.cl>
2017-10-17 23:48:09 -0300

    Salary range dropdown

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016031922) do
+ActiveRecord::Schema.define(version: 20171018022121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,18 @@ ActiveRecord::Schema.define(version: 20171016031922) do
     t.date "due_date"
     t.text "description"
     t.text "extra_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prospects", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "rut"
+    t.string "cellphone"
+    t.string "email"
+    t.integer "salary"
+    t.boolean "preapproval"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
