b2c7995cfa20edc18274202b0058d033d78e6041
Raimundo Herrera <rjherrera@uc.cl>
2017-11-12 23:29:49 -0300

    some changes in layout and added definitive phrases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-    array =* (1..8)
+    array =* (1..3)
     @projects = []
     array.each {@projects.push(Project.first())}
     render 'home'
